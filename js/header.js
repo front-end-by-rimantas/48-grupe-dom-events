59cc8c6f144b151098040a5876672add234acd26
Rimantas <belauzas@gmail.com>
2024-02-27 10:56:32 +0200

    Href update

--- js/header.js
+++ js/header.js
@@ -1,11 +1,13 @@
 function header() {
     const navDOM = document.querySelector('.main-nav');
+    const href = location.hostname === 'localhost' ? '/' : './';
+
     const HTML = `
-        <a href="/">Home</a>
-        <a href="/single-button-click">Single button click</a>
-        <a href="/counter/">Counter</a>
-        <a href="/basketball/">Basketball</a>
-        <a href="/virykle/">Virykle</a>
+        <a href="${href}">Home</a>
+        <a href="${href}single-button-click">Single button click</a>
+        <a href="${href}counter/">Counter</a>
+        <a href="${href}basketball/">Basketball</a>
+        <a href="${href}virykle/">Virykle</a>
     `;
     navDOM.innerHTML = HTML;
 }
