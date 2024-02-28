9e95de36247d89f4c55d63fc5fb2aab9b0fbd9b9
Rimantas <belauzas@gmail.com>
2024-02-28 08:44:38 +0200

    Href fix

--- js/header.js
+++ js/header.js
@@ -1,13 +1,15 @@
 function header() {
     const navDOM = document.querySelector('.main-nav');
-    const href = location.hostname === 'localhost' ? '/' : './';
+    const localhost = 'http://localhost:4810';
+    const github = 'https://front-end-by-rimantas.github.io/48-grupe-dom-events';
+    const href = location.hostname === 'localhost' ? localhost : github;
 
     const HTML = `
-        <a href="${href}">Home</a>
-        <a href="${href}single-button-click">Single button click</a>
-        <a href="${href}counter/">Counter</a>
-        <a href="${href}basketball/">Basketball</a>
-        <a href="${href}virykle/">Virykle</a>
+        <a href="${href}/">Home</a>
+        <a href="${href}/single-button-click">Single button click</a>
+        <a href="${href}/counter/">Counter</a>
+        <a href="${href}/basketball/">Basketball</a>
+        <a href="${href}/virykle/">Virykle</a>
     `;
     navDOM.innerHTML = HTML;
 }
