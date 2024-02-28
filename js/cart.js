5b9769c410ca6d82a71dec9097de16b7a5eb66f7
Rimantas <belauzas@gmail.com>
2024-02-28 09:24:44 +0200

    Shopping cart design

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,20 @@
 import { header } from './header.js';
 header();
 
+const data = [
+    {
+        name: 'Duona',
+        unitPrice: 1,
+        count: 1,
+    },
+    {
+        name: 'Pienas',
+        unitPrice: 0.89,
+        count: 2,
+    },
+    {
+        name: 'Cepelinai',
+        unitPrice: 2.89,
+        count: 5,
+    },
+];
